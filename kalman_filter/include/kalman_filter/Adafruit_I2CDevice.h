29335056ddc41a1ed07544e8e3f6086b3627df8d
Sharwin24 <sharwin24@gmail.com>
2025-02-18 11:33:55 -0600

    Included stdint and uint8_t is back

--- kalman_filter/include/kalman_filter/Adafruit_I2CDevice.h
+++ kalman_filter/include/kalman_filter/Adafruit_I2CDevice.h
@@ -4,20 +4,22 @@
 // #include <Arduino.h>
 // #include <Wire.h>
 
+#include <stdint.h>
+
 ///< The class which defines how we will talk to this device over I2C
 class Adafruit_I2CDevice {
 public:
-  Adafruit_I2CDevice(uint8 addr, TwoWire *theWire = &Wire);
-  uint8 address(void);
+  Adafruit_I2CDevice(uint8_t addr, TwoWire *theWire = &Wire);
+  uint8_t address(void);
   bool begin(bool addr_detect = true);
   void end(void);
   bool detected(void);
 
-  bool read(uint8 *buffer, size_t len, bool stop = true);
-  bool write(const uint8 *buffer, size_t len, bool stop = true,
-             const uint8 *prefix_buffer = nullptr, size_t prefix_len = 0);
-  bool write_then_read(const uint8 *write_buffer, size_t write_len,
-                       uint8 *read_buffer, size_t read_len,
+  bool read(uint8_t *buffer, size_t len, bool stop = true);
+  bool write(const uint8_t *buffer, size_t len, bool stop = true,
+             const uint8_t *prefix_buffer = nullptr, size_t prefix_len = 0);
+  bool write_then_read(const uint8_t *write_buffer, size_t write_len,
+                       uint8_t *read_buffer, size_t read_len,
                        bool stop = false);
   bool setSpeed(uint32_t desiredclk);
 
@@ -26,11 +28,11 @@ public:
   size_t maxBufferSize() { return _maxBufferSize; }
 
 private:
-  uint8 _addr;
+  uint8_t _addr;
   TwoWire *_wire;
   bool _begun;
   size_t _maxBufferSize;
-  bool _read(uint8 *buffer, size_t len, bool stop);
+  bool _read(uint8_t *buffer, size_t len, bool stop);
 };
 
 #endif // Adafruit_I2CDevice_h
