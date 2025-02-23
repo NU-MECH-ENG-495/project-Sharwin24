5e9c7631c99913fd33cea92d26d0075fcb626ff6
Sharwin24 <sharwin24@gmail.com>
2025-02-22 21:40:50 -0600

    wrote test file

---
+++ kalman_filter/src/imu_test.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+#include <cstdint>
+
+// Sensor Hub Transport Protocol
+// #include "sh2.h"
+
+#define I2C_DEVICE "/dev/i2c-1" // I2C Bus on Raspberry Pi 5
+#define IMU_ADDR 0x4A // I2C address of the BN0O85 IMU
+
+uint8_t CARGO_NO = 0x00; // SHTP Sequence Number
+uint8_t START_BNO_ALGO[21] = { 
+  0x15, 0x00, 0x02, CARGO_NO, 0xFD, 0x28, 0x00,
+  0x00, 0x00, 0x20, 0x4e, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+// Stream orientation as ARVR-Stabilized Rotation Vector (0x28) at 50 Hz (20us = 0x4e20)
+
+
+// 0xFD = SET_FEATURE_CMD
+// uint8_t START_BNO_ALGO[21] = { 
+//   SH2_RAW_GYROSCOPE, 0x00, SH2_GYROSCOPE_CALIBRATED, CARGO_NO, 0xFD, SH2_ARVR_STABILIZED_RV, 
+//   0x00, 0x00, 0x00, SH2_TILT_DETECTOR, 0x4e,
+//   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 
+// };
+
+
+// Open I2C communication
+int openI2CBus() {
+  int file = open(I2C_DEVICE, O_RDWR);
+  if (file < 0) {
+    std::cerr << "Failed to open I2C bus\n";
+    return -1;
+  }
+  if (ioctl(file, I2C_SLAVE, IMU_ADDR) < 0) {
+    std::cerr << "Failed to connect to I2C device\n";
+    return -1;
+  }
+  return file;
+}
+
+// Write data to a register
+bool i2cWrite(int file, uint8_t reg, uint8_t* data, size_t len) {
+  uint8_t buffer[len + 1];
+  buffer[0] = reg;
+  for (size_t i = 0; i < len; i++) {
+    buffer[i + 1] = data[i];
+  }
+  return write(file, buffer, len + 1) == len + 1;
+}
+
+// Read data from a register
+bool i2cRead(int file, uint8_t reg, uint8_t* data, size_t len) {
+  if (write(file, &reg, 1) != 1) {
+    std::cerr << "Error setting register address\n";
+    return false;
+  }
+  return read(file, data, len) == len;
+}
+
+int main() {
+  int file = openI2CBus();
+  if (file < 0) return 1;
+
+  // Configure IMU to stream data
+  i2cWrite(file, 0x00, START_BNO_ALGO, 21);
+  // Read data and print stream
+
+  close(file);
+  return 0;
+}
